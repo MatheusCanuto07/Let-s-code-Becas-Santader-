a5a107a6d7bb0338bfd452b365888ea45ef9f0e3
MatheusCanuto07 <70597769+MatheusCanuto07@users.noreply.github.com>
2021-07-13 17:16:36 -0300

    Add files via upload

---
+++ Let's code/Into-componentes.js
@@ -0,0 +1,3 @@
+//divisão da pagina em um funcionalidade / div diferente / Componente
+//React tem dois tipos de componentes
+//Funcionais e Tipo classe
---
+++ Let's code/introducao.js
@@ -0,0 +1,9 @@
+//O React também inaugura o conceito de programação reativa em frontend. Que basicamente é a reconstrução da página sempre que houver mudança nos dados. 
+//Isso é feito de uma forma muito inteligente pois ele é capaz de renderizar apenas as partes da página que são dependentes dos dados que foram modificados 
+//e não o todo, tendo por resultado um ótimo desempenho.
+
+//Para isso, o React usa uma arquitetura de componentes. 
+//Grande parte da programação em React está em saber dividir bem uma página em componentes e fazê-los de forma reutilizável e que admita composição com outros componentes.
+
+//React foi criado pelo proprio facebook, essa biblioteca da a impressão de atualização constante de pagina quando
+//Na verdade o usuário está parado na mesma página.
